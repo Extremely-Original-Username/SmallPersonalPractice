ad77927631a8f830e5e2a90aab2d361c9f0a376c
Josh Innes <jinnes@pulsion.co.uk>
2022-10-12 14:58:01 +0100

    reintroduction (done to fix error)

---
+++ GOL/GOL/IGameDrawer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GOL
+{
+    internal interface IGameDrawer
+    {
+        void draw(FixedGrid<int> grid, GameSettings gameSettings);
+    }
+}
---
+++ GOL/GOL/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using GOL;
+
+GameSettings settings = new GameSettings(50, 30, new string[] /*{"  ", " -", " =", " #"}*/ {"  ", " .", " o", " O", " @" }, 80);
+
+GameOfLife game = new GameOfLife(settings, new ExampleGameDrawer());
+game.drawGame();
+game.run();
